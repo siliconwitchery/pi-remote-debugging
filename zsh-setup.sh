14502ad9c0eca4c1bf6cf0fc770394907d542034
Raj Nakarja <siliconwitch@users.noreply.github.com>
2021-02-15 00:18:27 +0100

    fixed quotes

--- zsh-setup.sh
+++ zsh-setup.sh
@@ -15,7 +15,7 @@ sudo apt -y install zsh
 
 # Install oh-my-zsh
 
-sh -c “$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)”
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 
 # Install a nice theme. Change this if you want
