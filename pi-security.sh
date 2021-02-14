61891f55a116937f7cefca946eaedee58ab8cdb7
Raj Nakarja <siliconwitch@users.noreply.github.com>
2021-02-14 17:04:46 +0100

    changed strings again

--- pi-security.sh
+++ pi-security.sh
@@ -1,25 +1,27 @@
 #!/bin/sh
 # Exit on error
 set -e
-infotag='[\033[0;32mINFO\033[0;0m]'
 
 # Install cryptsetup if it isn't installed already
-echo "${infotag} Checking if cryptsetup is installed"
+echo "\nChecking if cryptsetup is installed\n"
 if ! command -v cryptsetup
 then
     sudo apt -y install cryptsetup
-    echo "${infotag} Cryptsetup installed. Re-run this script after reboot.."
+    echo "\nCryptsetup installed. Re-run this script after reboot"
     sudo reboot now
     exit
 fi
 
 
 # Asks the user to change the default password
-passwd
+if sudo passwd --status pi | grep NP
+then
+    passwd
+fi
 
 
 # Install a firewall and allow SSH access only
-echo "${infotag} Setting up firewall"
+echo "\nSetting up firewall\n"
 sudo apt -y install ufw
 sudo ufw allow ssh
 sudo ufw --force enable
@@ -39,18 +41,18 @@ sudo ufw --force enable
 #      Pi home directory.
 
 # Allocate an empty file which will become our secure disk
-read -p "${infotag} Specify a size for your encrypted home folder in \
-gigabytes (enter a number, eg. 8): " sec_flie_size
+echo "\nSpecify a size for your encrypted home folder in gigabytes"
+read -p "enter a number, eg. 8: " sec_flie_size
 sudo fallocate -l ${sec_flie_size}G /crypt-home-data
 sudo dd if=/dev/zero of=/crypt-home-data bs=1M count=${sec_flie_size}k status=progress
 
 # Encrypt the file we made
-echo "${infotag} Encrypting folder, answer YES to the question and \
+echo "\nEncrypting folder, answer YES to the question and \
 create a password for the encrypted folder."
 sudo cryptsetup -y luksFormat /crypt-home-data
 
 # Open and mount as a mapped disk
-echo "${infotag} Done. Enter the password again to mount and format the folder"
+echo "\nDone. Enter the password again to mount and format the folder\n"
 sudo cryptsetup luksOpen /crypt-home-data crypt-home
 
 # Format the drive
@@ -64,7 +66,7 @@ cd ~
 $(cat ~/.profiles)" > ~/.profiles
 
 # Reboot
-echo "${infotag} Security configured. Rebooting.."
+echo "\nSecurity configured. Rebooting.."
 sudo reboot now
 
 
