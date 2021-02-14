abd352933d545fa0b8242542a2b97bb55688e4a0
Raj Nakarja <siliconwitch@users.noreply.github.com>
2021-02-14 17:36:45 +0100

    cleanup

--- pi-security.sh
+++ pi-security.sh
@@ -1,27 +1,45 @@
 #!/bin/sh
-# Exit on error
+
+# Exits on error and enable echo escape flags
+
 set -e
 
+
+
 # Install cryptsetup if it isn't installed already
+
 echo "\nChecking if cryptsetup is installed\n"
+
 if ! command -v cryptsetup
 then
+    # Install
     sudo apt -y install cryptsetup
     echo "\nCryptsetup installed. Re-run this script after reboot"
+    
+    # Reboot
     sudo reboot now
     exit
 fi
 
 
-# Asks the user to change the default password
+
+# Asks the user to change the default 'raspberry' password
+
 if sudo passwd --status pi | grep NP
 then
     passwd
 fi
 
 
+
 # Install a firewall and allow SSH access only
+#
+# Note: If you need to enable more ports, you should add them
+#       using 'ufw allow <port_num>' and then re-enable using
+#       'ufw enable'
+
 echo "\nSetting up firewall\n"
+
 sudo apt -y install ufw
 sudo ufw allow ssh
 sudo ufw --force enable
@@ -40,9 +58,9 @@ sudo ufw --force enable
 #      drive, encrypt it, and then mount it over the existing
 #      Pi home directory.
 
-# Stop if the /crypt-home-data folder already exists
 if test -f "/crypt-home-data"
 then
+    # Stop if the /crypt-home-data folder already exists
     echo "/nEncrypted file already exists. Stopped to prevent /
     overwriting. If you want to resetup this file, back it up /
     with 'sudo mv /crypt-home-data /crypt-home-data.bak' or / 
@@ -51,38 +69,41 @@ then
 fi
 
 # Allocate an empty file which will become our secure disk
+
 echo "\nSpecify a size for your encrypted home folder in gigabytes"
 read -p "enter a number, eg. 8: " sec_flie_size
 sudo fallocate -l ${sec_flie_size}G /crypt-home-data
 sudo dd if=/dev/zero of=/crypt-home-data bs=1M count=${sec_flie_size}k status=progress
 
 # Encrypt the file we made
+
 echo "\nEncrypting folder, answer YES to the question and \
 create a password for the encrypted folder."
 sudo cryptsetup -y luksFormat /crypt-home-data
 
 # Open and mount as a mapped disk
+
 echo "\nDone. Enter the password again to mount and format the folder\n"
 sudo cryptsetup luksOpen /crypt-home-data crypt-home
 
 # Format the drive
+
 sudo mkfs.ext4 -j /dev/mapper/crypt-home
 
 # Append .profiles to automatically load the encrypted disk
 # at startup
+
 echo "Updating user ~/.profile"
 echo "sudo cryptsetup luksOpen /crypt-home-data crypt-home
 sudo mount /dev/mapper/crypt-home /home/pi
 cd ~
 $(cat ~/.profile)" > ~/.profile
 
+
 # Reboot
-echo "\nSecurity configured. Rebooting.."
-sudo reboot now
-
-
+#
 # Note: It's possible to skip mounting the new pi folder at
 #       login by pressing <Ctrl-C> at the unlock prompt.
 
-
-# TODO SSH key login
+echo "\nSecurity configured. Rebooting.."
+sudo reboot now
--- update-pi.sh
+++ update-pi.sh
@@ -1,18 +1,27 @@
 #!/bin/sh
 
-# Exit on error
+# Exit on error and enable echo escape flags
+
 set -e
 
+
 # Fully update the Pi
+
 sudo apt update
 sudo apt -y full-upgrade
 
+
 # Install a few extra tools for later
+
 sudo apt -y install git tmux cryptsetup
 
+
 # Cleanup old packages
+
 sudo apt -y autoremove
 
+
 # And reboot to start any new services
-printf "\nEverything updated. Rebooting.."
+
+echo "\nEverything updated. Rebooting.."
 sudo reboot
