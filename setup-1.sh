30a6bd316aeccacf6c131074b8ac4aea1bddaaa9
Raj <raj@siliconwitchery.com>
2021-02-19 16:05:19 +0100

    Split into two files

---
+++ setup-1.sh
@@ -0,0 +1,117 @@
+#!/bin/sh
+
+# Usage: 
+# ------
+#   Run this script first using `sh setup-1.sh`, then run setup-2.sh after
+#   the automatic reboot. NOTE: Avoid loosing SSH connection as this 
+#   script takes a while. It will abort if SSH is lost. Use something like
+#   TMUX to keep sessions alive if your connection is flakey.
+#
+# Brief:
+# ------
+#   A Raspbian linux configuration for building ARM embedded applications
+#   and flashing Cortex M targets using a J-Link debugger all from a 
+#   Rapsberry Pi 4.
+#
+#   This configuration includes basic build and flash tools, as well as a
+#   good starting IDE like setup for developing C/C++ Code.
+#
+#   Additionally, the pi home folder will be encrypted using LUKs based 
+#   encryption to ensure safe storage of user data and sensitive
+#   information.
+#
+# Major tools installed:
+# ----------------------
+#   - git
+#   - tmux
+#   - zsh
+#   - neovim (+ some plugins & config)
+#   - ARM GCC toolchain for Cortex R/M
+#   - J-Link tools
+#
+# Author(s):
+# -------
+#   Raj Nakarja | Silicon Witchery AB
+#
+
+# Exit on error and enables echo escape characters
+set -e
+
+
+# Update repos, run a full update, and cleanup. Takes a while
+sudo apt update
+sudo apt -y full-upgrade
+sudo apt -y autoremove
+
+
+# Install tools:
+#   git             For version control
+#   tmux            Multitasking inside the terminal
+#   cryptsetup      Encrypting files
+#   neovim          A nice code editor
+#   nodejs          Require for some nvim tools
+#   npm             We need yarn for some nvim tools
+#   clang-tools     Required for autocompletion/intellisense
+#   ripgrep         A better file search, used by nvim plugins
+#   fzf             A fuzzy file search, used by nvim plugins
+#   libncurses5     Required to run GDB on ARM64
+#   zsh             A nicer shell
+#
+sudo apt -y install git tmux cryptsetup neovim nodejs npm clang-tools \
+    ripgrep fzf libncurses5 zsh
+
+
+# We'll keep ARM and J-Link tools in this directory
+sudo mkdir -p /tools
+
+
+# Download the ARM GCC toolchain
+curl -fSL https://developer.arm.com/-/media/Files/downloads/gnu-rm/10-2020q4/\
+gcc-arm-none-eabi-10-2020-q4-major-aarch64-linux.tar.bz2 \
+--output gcc-arm-none-eabi.bz2
+
+
+# Extract, move and clean up the archive
+tar -xf gcc-arm-none-eabi.bz2
+sudo mv gcc-arm-none-eabi-*/ /tools/gcc-arm-none-eabi
+rm gcc-arm-none-eabi.bz2
+
+
+# Download the J-LINK tools
+curl -fSL -X POST -d \
+'accept_license_agreement=accepted&non_emb_ctr=confirmed&submit=Download+\
+software' https://www.segger.com/downloads/jlink/JLink_Linux_arm64.tgz \
+--output JLink_Linux_arm64.tgz
+
+
+# Extract, move and clean up the archive
+tar -xf JLink_Linux_arm64.tgz
+sudo mv JLink_Linux_V* /tools/jlink
+rm JLink_Linux_arm64.tgz
+
+
+# Enable J-Link USB driver by copying the rule file to udev
+sudo cp /tools/jlink/99-jlink.rules /etc/udev/rules.d/
+
+
+# Reboot
+sudo reboot
+
+
+# Licence:
+# --------
+#   These instructions and scripts are released unencumbered into the public 
+#   domain.
+#
+#   Feel free to use this information as a starting point, suggest
+#   improvements, or fork this repository so that others may find your version.
+#
+#   All other software installed using these scripts remain under the terms of 
+#   their respective licenses.
+#
+#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+#   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+#   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+#   AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN 
+#   ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+#   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
