2269e251b07e9b2831a887a2d85bfd55c29aa12a
Raj <raj@siliconwitchery.com>
2021-02-19 14:23:52 +0100

    Cleanup

--- setup.sh
+++ setup.sh
@@ -4,22 +4,15 @@
 set -e
 
 
-# Update repos
+# Update repos, run a full update, and cleanup. Takes a while
 sudo apt update
-
-
-# Install TMUX and start so we don't loose the session of SSH disconnects
-sudo apt -y install tmux
-tmux
-
-
-# Do a full update, and clean up old packages. This can take a while
 sudo apt -y full-upgrade
 sudo apt -y autoremove
 
 
-# Install these additional tools:
+# Install tools:
 #   git             For version control
+#   tmux            Multitasking inside the terminal
 #   cryptsetup      Encrypting files
 #   ufw             A simple firewall
 #   neovim          A nice code editor
@@ -31,7 +24,7 @@ sudo apt -y autoremove
 #   libncurses5     Required to run GDB on ARM64
 #   zsh             A nicer shell
 #
-sudo apt -y install git cryptsetup ufw neovim nodejs npm clang-tools \
+sudo apt -y install git tmux cryptsetup ufw neovim nodejs npm clang-tools \
     ripgrep fzf libncurses5 zsh
 
 
@@ -117,16 +110,13 @@ echo "\nAnswer YES to the question and create a password for the folder."
 sudo cryptsetup -y luksFormat /crypt-home-data
 
 
-# Open and mount as a mapped disk
+# Open and mount the file as a mapped disk, then format it
 echo "\nDone. Enter the password again to mount and format the folder\n"
 sudo cryptsetup luksOpen /crypt-home-data crypt-home
-
-
-# Format the drive
 sudo mkfs.ext4 -j /dev/mapper/crypt-home
 
 
-# Append .profile and .zprofile to automatically load the disk at startup
+# .profile and .zprofile will automatically load the disk at first login
 cat <<EOF > /home/pi/.profile
 sudo cryptsetup luksOpen /crypt-home-data crypt-home
 sudo mount /dev/mapper/crypt-home /home/pi
@@ -136,41 +126,45 @@ EOF
 ln -s .profile .zprofile
 
 
-# Mount the new encrypteddrive
+# Mount the new encrypted drive
 sudo mount /dev/mapper/crypt-home /home/pi                                                                                                    
 sudo chown pi:pi /home/pi                                                                                                                     
 cd ~ 
 
 
-# Get a nice starting vim configuration.
-git clone https://github.com/siliconwitch/nvim-init.git ~/.config/nvim
+# Grab our vim configuration and symlink it to the nvim folder
+git clone https://github.com/siliconwitch/nvim-init.git ~/projects/nvim-init
 mkdir -p ~/.config/nvim
 ln -s ~/projects/nvim-init/init.vim ~/.config/nvim/init.vim
 
 
-# Install plug for nvim
+# Install the plug plugin manager for nvim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 
-# Install neovim plugins
-echo "Installing plugins\n" 
+# Install neovim plugins and CoC Plugins
 nvim +PlugInstall -c "CocInstall -sync coc-clangd coc-explorer" +qall
 
 
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-# Download a theme and create symlink
+
+# Download a theme and create symlink for zsh to load it
 git clone https://github.com/dracula/zsh.git ~/projects/dracula
 ln -s ~/projects/dracula/dracula.zsh-theme ~/.oh-my-zsh/themes/dracula.zsh-theme
 
 
 # Download custom zsh configuration and apply as default shell
-
+git clone https://github.com/siliconwitch/zsh-config.git ~/projects/zsh-config
+ln -s ~/projects/zsh-config/.zshrc ~/.zshrc
 sudo chsh -s $(which zsh) pi
 
 
 # Download custom tmux configuration 
+git clone https://github.com/siliconwitch/tmux-config.git ~/projects/tmux-config
+ln -s ~/projects/zsh-config/.tmux.conf ~/.tmux.conf
+
 
 # Reboot
 sudo reboot
